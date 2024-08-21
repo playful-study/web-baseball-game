c9b670d7ac38701b687f804642c187bdb5b7a83d
junho <>
2024-08-21 17:23:20 +0900

    fix: acvitve 코드 수정

--- front/baseball/src/component/navbar/LinkNavbar.jsx
+++ front/baseball/src/component/navbar/LinkNavbar.jsx
@@ -20,7 +20,7 @@ const StyledNavLink = styled(NavLink)`
   padding: 0.5em;
   color: #0E201F;
   margin-right: 1.2em;
-  
+
   &.active {
     font-weight: bold;
     color: #ffffff; /* 활성화된 링크의 색상 */
@@ -33,38 +33,33 @@ const LinkNavbar = () => {
 
   if (!location.pathname.startsWith('/gameRoom')) {
     return (
-        <StyledLinkNavbar>
-            <StyledLinkDiv>
-                <StyledNavLink 
-                    to="/lobby/pvp" 
-                    isActive={() => location.pathname.startsWith('/lobby')}
-                    activeClassName="active"
-                >
-                    PVP
-                </StyledNavLink>
-                <StyledNavLink 
-                    to="/lobby/pve" 
-                    isActive={() => location.pathname.startsWith('/lobby')}
-                    activeClassName="active"
-                >
-                    PVE
-                </StyledNavLink>
-                <StyledNavLink 
-                    to="/lobby/ranking" 
-                    isActive={() => location.pathname.startsWith('/lobby')}
-                    activeClassName="active"
-                >
-                    랭킹보기
-                </StyledNavLink>
-            </StyledLinkDiv>
-          
-          <RankingBar/>
-        </StyledLinkNavbar>
-    )
+      <StyledLinkNavbar>
+        <StyledLinkDiv>
+          <StyledNavLink
+            to="/lobby/pvp"
+            className={({ isActive }) => (isActive ? 'active' : '')}
+          >
+            PVP
+          </StyledNavLink>
+          <StyledNavLink
+            to="/lobby/pve"
+            className={({ isActive }) => (isActive ? 'active' : '')}
+          >
+            PVE
+          </StyledNavLink>
+          <StyledNavLink
+            to="/lobby/ranking"
+            className={({ isActive }) => (isActive ? 'active' : '')}
+          >
+            랭킹보기
+          </StyledNavLink>
+        </StyledLinkDiv>
+        <RankingBar />
+      </StyledLinkNavbar>
+    );
   } else {
     return null;
   }
-
 };
 
 export default LinkNavbar;
