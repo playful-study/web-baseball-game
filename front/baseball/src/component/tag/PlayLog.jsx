6f519c830c32b950ea5adf03088d90285bb869bd
junho <>
2024-09-05 17:48:14 +0900

    feat: pve 화면 제작 완료(서버 연동 전)

--- front/baseball/src/component/tag/PlayLog.jsx
+++ front/baseball/src/component/tag/PlayLog.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { UserContext } from '../../context/UserContext';
 
 const StyledPlayLog = styled.div`
-  
+  width: 80%;
+  height: 70%;
+  background-color: #ffffff;
+  border-radius: 1em;
+  box-sizing: border-box;
+  margin: auto;
+  padding: 2em;
+  overflow-y: auto;
+
+  & > span {
+    display: block;
+    text-align: center;
+    font-size: 1.1em;
+    font-weight: bold;
+  }
 `;
 
-const PlayLog = () => {
+const PlayLog = ({ player, logs }) => {
+  const { user } = useContext(UserContext);
+  const logName = player === user ? '나' : player;
   return (
     <StyledPlayLog>
-      
+      <span>{logName.trim()}의 기록</span>
+      <div>
+        {}
+      </div>
     </StyledPlayLog>
   )
 }
